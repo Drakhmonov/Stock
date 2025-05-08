b90d76e74dd74d709f4af095163a729482ade0b2
Dilmurod Rakhmonov <dilmurod.rakhmonov01@gmail.com>
2025-05-08 11:21:26 +0100

    Initial Commit

---
+++ StockTests/StockTests.swift
@@ -0,0 +1,17 @@
+//
+//  StockTests.swift
+//  StockTests
+//
+//  Created by Dilmurod Rakhmonov on 08/05/2025.
+//
+
+import Testing
+@testable import Stock
+
+struct StockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
